17c077ed50a8f705d8d74a14e3dfef532edc061c
Wraithshroud <jele0006@algonquinlive.com>
2017-12-22 22:32:31 -0500

    commit
    adding project

---
+++ js/starter.js
@@ -0,0 +1,8 @@
+
+let contactStarter = [
+    {"fullname":"Valeera Sanguinar","phone":"613-727-4723","email":"valeera.rogue@theuncrowned.com"},
+    {"fullname":"Illidan Stormrage","phone":"613-321-8765","email":"illidan.stormrage@demonhunter.org"},
+    {"fullname":"Nova Terra","phone":"613-876-9400","email":"nova.terra@ghost.io"},
+    {"fullname":"Varian Wrynn","phone":"613-473-2581","email":"varian.king@thealliance.ca"},
+    {"fullname":"Tychus Findlay","phone":"613-410-5507","email":"tychus.findlay@outlaw.com"}
+];
